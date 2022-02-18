8dd41143abbb5f1ff116869aeac8074c3029d1ce
Kristen Thyng <kristen@axds.co>
2022-02-18 15:49:03 -0600

    init file and more precommit items

--- model_catalogs/model_catalogs/__init__.py
+++ model_catalogs/model_catalogs/__init__.py
@@ -7,8 +7,16 @@ import shutil
 
 from glob import glob
 
+from pkg_resources import DistributionNotFound, get_distribution
+
 from .model_catalogs import Management, agg_for_date, make_catalog  # noqa
 
+
+try:
+    __version__ = get_distribution("model_catalogs").version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "unknown"
 
 # make directories
 # os.makedirs('.catalogs', exist_ok=True)
