4177add6064a2ba9232a0f4ed37ace25e4c3675d
Kristen Thyng <kristen@axds.co>
2022-03-16 14:52:34 -0500

    updated init to match rearrangement

--- model_catalogs/model_catalogs/__init__.py
+++ model_catalogs/model_catalogs/__init__.py
@@ -9,8 +9,12 @@ from glob import glob
 
 from pkg_resources import DistributionNotFound, get_distribution
 
-from .model_catalogs import (setup_source_catalog, agg_for_date, make_catalog,
-                             complete_source_catalog, update_catalog, add_url_path)  # noqa
+from .model_catalogs import (setup_source_catalog, make_catalog,
+                             complete_source_catalog,
+                             add_url_path)  # noqa
+
+from .utils import (find_bbox, agg_for_date, find_catrefs, find_filelocs,
+                    get_dates_from_ofs, find_availability)
 
 
 try:
@@ -23,29 +27,26 @@ except DistributionNotFound:
 # set up known locations for catalogs. Can be overwritten HOW
 CATALOG_PATH = f"{__path__[0]}/catalogs"
 SOURCE_CATALOG_NAME = "source_catalog.yaml"
-CAT_SOURCE_BASE = f"{CATALOG_PATH}/source_catalogs"
-CAT_USER_BASE = f"{CATALOG_PATH}/user_catalogs"
 
-SOURCE_CATALOG_DIR_ORIG = f"{CAT_SOURCE_BASE}/orig"
-SOURCE_CATALOG_DIR = f"{CAT_SOURCE_BASE}/complete"
+CATALOG_PATH_DIR_ORIG = f"{CATALOG_PATH}/orig"
+CATALOG_PATH_DIR = f"{CATALOG_PATH}/complete"
+CATALOG_PATH_UPDATED = f"{CATALOG_PATH}/updated"
 
 # make directories
-# os.makedirs('.catalogs', exist_ok=True)
-# os.makedirs('.catalogs/source_catalogs', exist_ok=True)
-# os.makedirs(f"{__path__[0]}/catalogs/updated_catalogs", exist_ok=True)
-os.makedirs(f"{__path__[0]}/catalogs/user_catalogs", exist_ok=True)
-#
+os.makedirs('.catalogs', exist_ok=True)
+os.makedirs(f"{__path__[0]}/catalogs/updated", exist_ok=True)
+
 # set up testing
 os.makedirs(f"{__path__[0]}/tests/catalogs", exist_ok=True)
-# copy source catalogs to tests
-os.makedirs(f"{__path__[0]}/tests/catalogs/source_catalogs/", exist_ok=True)
-os.makedirs(f"{__path__[0]}/tests/catalogs/source_catalogs/orig", exist_ok=True)
+# # copy source catalogs to tests
+# os.makedirs(f"{__path__[0]}/tests/catalogs/source_catalogs/", exist_ok=True)
+# os.makedirs(f"{__path__[0]}/tests/catalogs/source_catalogs/orig", exist_ok=True)
 [
     shutil.copy(fname, f"{__path__[0]}/tests/catalogs/source_catalogs/orig/")
     for fname in glob(f"{__path__[0]}/catalogs/source_catalogs/orig/*")
 ]
 # also copy transform template
 shutil.copy(
-    f"{__path__[0]}/catalogs/source_catalogs/transform.yaml",
-    f"{__path__[0]}/tests/catalogs/source_catalogs/",
+    f"{__path__[0]}/catalogs/transform.yaml",
+    f"{__path__[0]}/tests/catalogs/",
 )
