a250686981246c7bf80bb4a2e49630e7f97c8e4e
Kristen Thyng <kristen@axds.co>
2022-08-16 16:35:45 -0500

    hopeful fix for ci

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     use_scm_version={
         "root": "..",  # this is because the .git dir is back a dir
         "relative_to": __file__,
-        "write_to": "model_catalogs/model_catalogs/_version.py",
+        "write_to": "model_catalogs/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     }
