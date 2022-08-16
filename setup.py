0a86513714b11808eecf1fb6d45d8525913b1945
Kristen Thyng <kristen@axds.co>
2022-08-16 16:45:18 -0500

    there was another fix to do still

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     use_scm_version={
-        "root": "..",  # this is because the .git dir is back a dir
+        # "root": "..",  # this is because the .git dir is back a dir
         "relative_to": __file__,
         "write_to": "model_catalogs/_version.py",
         "write_to_template": '__version__ = "{version}"',
