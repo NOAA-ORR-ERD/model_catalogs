f73e10b1308881b155896320ec7dbf75c5300c82
Kristen Thyng <kthyng@gmail.com>
2023-02-02 12:09:50 -0600

    series of fixes
    * The 3 linux tests should be fixed with mamba CI change
    * Also update release.yaml
    * also update where getting version from in __init__ and docs.conf.py
    * slow tests issue is also mamba CI change

--- model_catalogs/__init__.py
+++ model_catalogs/__init__.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import pandas as pd
 
 from appdirs import AppDirs
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 from .model_catalogs import (  # noqa
     find_availability,
@@ -34,8 +34,8 @@ from .utils import (  # noqa
 
 
 try:
-    __version__ = get_distribution("model_catalogs").version
-except DistributionNotFound:
+    __version__ = version("model_catalogs")
+except PackageNotFoundError:
     # package is not installed
     __version__ = "unknown"
 
